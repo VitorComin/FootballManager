cf8593dc36fba6d756528c6eeea90afffc352542
VITOR GABRIEL COMIN <vitor.comin@univille.br>
2023-04-03 21:13:16 -0300

    Projeto criado

---
+++ src/main/java/fabdesoft/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package fabdesoft.demo.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView index(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("apelido","valor");
+        map.put("nome","zezinho");
+        return new ModelAndView("home/index","apelido","valor");
+    }
+}
---
+++ src/main/java/fabdesoft/demo/entity/Cliente.java
@@ -0,0 +1,39 @@
+package fabdesoft.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 600)
+    private String nome;
+    private Date dataNascimento;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+}
